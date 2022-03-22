81e83b4d9e5e7aa8bdb6a9e3a5bcb524086d767a
Nicole Candiotti <nccandiotti@gmail.com>
2022-03-22 11:39:28 -0400

    commit tree 88880fe691282dab949d8e298f50d161fb9dbe92

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  get '/login', to: "sessions_controller#create"
+  get '/logout', to:  "sessions_controller#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
